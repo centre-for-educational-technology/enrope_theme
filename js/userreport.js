7e14a4164d0ee6f682378cfb0a5896d75c17948a
GlebRed <glebredko@gmail.com>
2018-12-20 17:24:53 +0200

    Closes #13, changed edit node layout

---
+++ js/userreport.js
@@ -0,0 +1,8 @@
+
+window._urq = window._urq || [];
+_urq.push(['initSite', 'da4a7285-bd7d-4db1-b9ac-1c8566dee64a']);
+(function() {
+  var ur = document.createElement('script'); ur.type = 'text/javascript'; ur.async = true;
+  ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur, s);
+})();
