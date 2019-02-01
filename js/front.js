8a7c73bc5bb94a03dacbbe006f0fba9beaaf0879
GlebRed <glebredko@gmail.com>
2019-02-01 13:52:45 +0200

    Closes #27

--- js/front.js
+++ js/front.js
@@ -2,25 +2,25 @@ var particles = Particles.init({
   selector: '.background',
   color: ['#24cc85', '#96ABFE', '#83CC6A'],
   connectParticles: true,
-  maxParticles: 150,
+  maxParticles: 100,
   sizeVariations: 20,
 
   responsive: [
     {
       breakpoint: 768,
       options: {
-        maxParticles: 100,
+        maxParticles: 40,
       }
     }, {
       breakpoint: 425,
       options: {
-        maxParticles: 50,
+        maxParticles: 30,
       }
 
     }, {
       breakpoint: 320,
       options: {
-        maxParticles: 25
+        maxParticles: 20
     }
   }
   ]
