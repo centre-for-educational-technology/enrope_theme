7a49b49d2ec537aa64fb71776cfd541a70abcd0c
GlebRed <glebredko@gmail.com>
2019-03-11 15:11:14 +0200

    References #35

--- js/group.js
+++ js/group.js
@@ -1,15 +1,14 @@
 (function ($, Drupal) {
 
-
-
   var image = $('#cover_image').html();
 
   if(image.includes('files')){
 
-    $('header.masthead.group:before').css('background-image', 'url('+image+')');
+    $('header.masthead.group').css('background-image', 'url('+image+')');
   }
 
 
-
-
 })(jQuery, Drupal);
+
+
+
