f8e52478676041d02154e6fcae973d549b1931ac
GlebRed <glebredko@gmail.com>
2020-02-16 21:31:32 +0000

    User match related

--- js/group.js
+++ js/group.js
@@ -1,10 +1,16 @@
 (function ($, Drupal) {
 
+
+  // Noty({
+  //   text: "Notification text"
+  // }).show();
+
   var image = $('#cover_image').html();
 
   if(image.includes('files')){
 
-    $('header.masthead.group').css('background-image', 'url('+image+')');
+
+    $('header.masthead.cover-img').css('background-image', 'url('+image+')');
   }
 
 
