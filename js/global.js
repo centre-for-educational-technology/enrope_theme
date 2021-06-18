21ca2a3f3710533754de2d0b3db10f7dfe33f242
GlebRed <glebredko@gmail.com>
2021-06-18 17:12:47 +0100

    :sparkles: Closes #154

--- js/global.js
+++ js/global.js
@@ -91,6 +91,18 @@
       }
 
 
+      $('.dropdown-menu a.dropdown-toggle', context).on('click', function(e) {
+        if (!$(this).next().hasClass('show')) {
+          $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
+        $(this).parents('li.nav-item.dropdown.show', context).on('hidden.bs.dropdown', function(e) {
+          $('.dropdown-submenu .show').removeClass("show");
+        });
+        return false;
+      });
+
     }
   };
 
